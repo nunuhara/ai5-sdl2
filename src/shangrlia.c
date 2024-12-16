50eb3c76d41566e432efb80a50a33bb4a3207a94
Nunuhara Cabbage <nunuhara@haniwa.technology>
2024-12-15 16:08:19 -0800

    shangrlia: partial map implementation
    Partial implementation of Util.function[0], which deals with the map,
    units, etc. Unfortunately most of the actual game is also implemented in
    native code as part of this util, so I am abandoning support for this
    game.

--- src/shangrlia.c
+++ src/shangrlia.c
@@ -16,13 +16,17 @@
 
 #include "nulib.h"
 #include "nulib/little_endian.h"
+#include "nulib/utfsjis.h"
 #include "ai5/mes.h"
 
 #include "anim.h"
 #include "asset.h"
+#include "char.h"
 #include "classics.h"
+#include "cursor.h"
 #include "game.h"
 #include "gfx.h"
+#include "input.h"
 #include "memory.h"
 #include "savedata.h"
 #include "sys.h"
@@ -93,11 +97,6 @@ static void shangrlia_savedata(struct param_list *params)
 	}
 }
 
-static void sys_22(struct param_list *params)
-{
-	WARNING("System.function[22] not implemented");
-}
-
 static void shangrlia_set_speaker(struct param_list *params)
 {
 	unsigned no = vm_expr_param(params, 0);
@@ -115,6 +114,321 @@ static void shangrlia_set_speaker(struct param_list *params)
 	}
 	if (no < 8)
 		anim_start(no);
+}
+
+// size of the map in tiles
+#define MAP_TW 19
+#define MAP_TH 11
+
+// size of a tile in pixels
+#define TILE_SIZE 32
+
+// top-left screen coordinate of map
+#define MAP_X 16
+#define MAP_Y 40
+
+#define NO_UNIT 0xff
+
+// tile coordinates (32x32) into CHIP2.GP8 for each tile index
+struct { uint8_t tx, ty; } tile_cg_coords[] = {
+	[0x00] = {  0, 0 },
+	[0x01] = {  4, 0 },
+	[0x02] = {  8, 0 },
+	[0x03] = { 12, 0 },
+	[0x04] = { 16, 0 },
+	[0x05] = {  0, 1 },
+	[0x06] = {  4, 1 },
+	[0x07] = {  8, 1 },
+	[0x08] = { 12, 1 },
+	[0x09] = { 16, 1 },
+	[0x0a] = {  0, 2 },
+	[0x10] = {  1, 0 },
+	[0x11] = {  5, 0 },
+	[0x12] = {  9, 0 },
+	[0x13] = { 13, 0 },
+	[0x14] = { 17, 0 },
+	[0x15] = {  1, 1 },
+	[0x16] = {  5, 1 },
+	[0x17] = {  9, 1 },
+	[0x18] = { 13, 1 },
+	[0x19] = { 17, 1 },
+	[0x1a] = {  1, 2 },
+	[0x20] = {  2, 0 },
+	[0x21] = {  6, 0 },
+	[0x22] = { 10, 0 },
+	[0x23] = { 14, 0 },
+	[0x24] = { 18, 0 },
+	[0x25] = {  2, 1 },
+	[0x26] = {  6, 1 },
+	[0x27] = { 10, 1 },
+	[0x28] = { 14, 1 },
+	[0x29] = { 18, 1 },
+	[0x30] = {  3, 0 },
+	[0x31] = {  7, 0 },
+	[0x32] = { 11, 0 },
+	[0x33] = { 15, 0 },
+	[0x34] = { 19, 0 },
+	[0x35] = {  3, 1 },
+	[0x36] = {  7, 1 },
+	[0x37] = { 11, 1 },
+	[0x38] = { 15, 1 },
+	[0x39] = { 19, 1 },
+};
+
+static struct {
+	uint8_t *map_ptr;
+	uint8_t *unit_ptr;
+	uint8_t *unitpara_ptr;
+	uint8_t *chikei_ptr;
+} mapdata;
+
+struct tile {
+	uint8_t tile_no;
+	uint8_t unit_no;
+};
+
+struct unit {
+	uint8_t present;
+	uint8_t index;
+	uint8_t tx;
+	uint8_t ty;
+	uint8_t uk5;
+};
+
+static struct unit units[64];
+
+static struct tile tilemap[MAP_TH][MAP_TW];
+
+static void load_map(uint8_t *map)
+{
+	memset(tilemap, 0, sizeof(tilemap));
+	if (le_get16(map, 1) != MAP_TW)
+		VM_ERROR("Unexpected map width: %u", le_get16(map, 1));
+	if (le_get16(map, 3) != MAP_TH)
+		VM_ERROR("Unexpected map height: %u", le_get16(map, 3));
+
+	uint8_t *p = map + 5;
+	for (int row = 0; row < MAP_TH; row++) {
+		for (int col = 0; col < MAP_TW; col++, p += 4) {
+			tilemap[row][col].tile_no = *p;
+			tilemap[row][col].unit_no = NO_UNIT;
+		}
+	}
+}
+
+static void load_unit(uint8_t *unit)
+{
+	for (int i = 0; i < 64; i++, unit += 8) {
+		units[i].present = unit[0] & 0xf;
+		units[i].index = unit[1];
+		units[i].tx = unit[3];
+		units[i].ty = unit[4];
+		units[i].uk5 = unit[5];
+	}
+}
+
+static void load_unitpara(uint8_t *unitpara)
+{
+
+}
+
+static void load_chikei(uint8_t *chikei)
+{
+
+}
+
+static void place_units(void)
+{
+	for (int i = 0; i < 64; i++) {
+		if (!units[i].present) {
+			continue;
+		}
+		struct unit *unit = &units[i];
+		struct tile *tile = &tilemap[unit->ty][unit->tx];
+		tile->unit_no = i;
+	}
+}
+
+static void update_map(void)
+{
+	load_map(mapdata.map_ptr);
+	load_unit(mapdata.unit_ptr);
+	load_unitpara(mapdata.unitpara_ptr);
+	load_chikei(mapdata.chikei_ptr);
+	place_units();
+}
+
+static void util_map_init(struct param_list *params)
+{
+	uint8_t *map = memory_raw + vm_expr_param(params, 2);
+	uint8_t *unit = memory_raw + vm_expr_param(params, 3);
+	uint8_t *unitpara = memory_raw + vm_expr_param(params, 4);
+	uint8_t *chikei = memory_raw + vm_expr_param(params, 5);
+	//unsigned uk1 = vm_expr_param(params, 6);
+	uint8_t *uk2 = memory_raw + vm_expr_param(params, 7);
+	uint8_t *uk3 = memory_raw + vm_expr_param(params, 8);
+	if (!mem_ptr_valid(map, 5 + MAP_TW * MAP_TH * 4))
+		VM_ERROR("Invalid map pointer: 0x%x", params->params[2].val);
+	if (!mem_ptr_valid(unit, 1))
+		VM_ERROR("Invalid unit pointer: 0x%x", params->params[3].val);
+	if (!mem_ptr_valid(unitpara, 1))
+		VM_ERROR("Invalid unitpara pointer: 0x%x", params->params[4].val);
+	if (!mem_ptr_valid(chikei, 1))
+		VM_ERROR("Invalid chikei pointer: 0x%x", params->params[5].val);
+	if (!mem_ptr_valid(uk2, 1))
+		VM_ERROR("Invalid uk2 pointer: 0x%x", params->params[7].val);
+	if (!mem_ptr_valid(uk3, 1))
+		VM_ERROR("Invalid uk3 pointer: 0x%x", params->params[8].val);
+	mapdata.map_ptr = map;
+	mapdata.unit_ptr = unit;
+	mapdata.unitpara_ptr = unitpara;
+	mapdata.chikei_ptr = chikei;
+	update_map();
+}
+
+static void draw_tile(int col, int row)
+{
+	uint8_t tile_no = tilemap[row][col].tile_no;
+	unsigned tile_ord = (tile_no & 0xf) * 4 + (tile_no >> 4);
+	int src_x = (tile_ord % 20) * TILE_SIZE;
+	int src_y = (tile_ord / 20) * TILE_SIZE;
+	int dst_x = MAP_X + col * TILE_SIZE;
+	int dst_y = MAP_Y + row * TILE_SIZE;
+	gfx_copy(src_x, src_y, TILE_SIZE, TILE_SIZE, 1, dst_x, dst_y, 0);
+
+	int unit_no = tilemap[row][col].unit_no;
+	if (unit_no == NO_UNIT)
+		return;
+
+	if (unit_no < 32) {
+		tile_ord = units[unit_no].index + 64;
+	} else {
+		tile_ord = units[unit_no].index + 42;
+		if (tile_ord >= 74)
+			tile_ord += 8; // ???
+	}
+	src_x = (tile_ord % 20) * TILE_SIZE;
+	src_y = (tile_ord / 20) * TILE_SIZE;
+	gfx_copy_masked(src_x, src_y, TILE_SIZE, TILE_SIZE, 1, dst_x, dst_y, 0, 0xf);
+}
+
+static void draw_tile_cursor(int tx, int ty)
+{
+	int dst_x = MAP_X + tx * TILE_SIZE;
+	int dst_y = MAP_Y + ty * TILE_SIZE;
+	gfx_copy_masked(0, 160, TILE_SIZE, TILE_SIZE, 1, dst_x, dst_y, 0, 0xf);
+}
+
+static void draw_map(void)
+{
+	for (int row = 0; row < MAP_TH; row++) {
+		for (int col = 0; col < MAP_TW; col++) {
+			draw_tile(col, row);
+		}
+	}
+}
+
+static int mouse_tx = 0;
+static int mouse_ty = 0;
+static int mouse_btn = 0;
+
+static void get_mouse_state(void)
+{
+	mouse_btn = 0;
+
+	unsigned x, y;
+	cursor_get_pos(&x, &y);
+	if (x >= MAP_X && x < 640 - MAP_X && y >= MAP_Y && y < 400 - 8) {
+		mouse_tx = (x - MAP_X) / TILE_SIZE;
+		mouse_ty = (y - MAP_Y) / TILE_SIZE;
+	}
+
+	if (input_down(INPUT_ACTIVATE)) {
+		mouse_btn = 1;
+	}
+	if (input_down(INPUT_CANCEL)) {
+		mouse_btn = 2;
+	}
+}
+
+static void handle_map_input(void)
+{
+	// TODO: draw selected info
+
+	do {
+		int prev_mouse_tx = mouse_tx;
+		int prev_mouse_ty = mouse_ty;
+		get_mouse_state();
+		if (prev_mouse_tx != mouse_tx || prev_mouse_ty != mouse_ty) {
+			// move tile cursor
+			draw_tile(prev_mouse_tx, prev_mouse_ty);
+			draw_tile_cursor(mouse_tx, mouse_ty);
+		}
+		vm_peek();
+		gfx_update();
+	} while (mouse_btn == 0);
+
+	if (mouse_btn == 1) {
+		mem_set_var16(18, (mouse_tx << 8) | mouse_ty);
+		mem_set_var16(19, mouse_tx);
+	} else {
+		mem_set_var16(18, 0xffff);
+	}
+}
+
+static void util_map(struct param_list *params)
+{
+	switch (vm_expr_param(params, 1)) {
+	case 0:
+		util_map_init(params);
+		break;
+	case 4:
+		update_map();
+		draw_map();
+		// TODO: draw_selected_info
+		break;
+	case 6:
+		draw_map();
+		break;
+	case 5:
+		handle_map_input();
+		break;
+	case 7:
+		load_map(mapdata.map_ptr);
+		load_unit(mapdata.unit_ptr);
+		place_units();
+		draw_map();
+		break;
+	case 1:
+		VM_ERROR("Util.Map.function[%d] not implemented", params->params[1].val);
+		break;
+	}
+}
+
+static void shangrlia_draw_text(const char *text)
+{
+	const uint16_t surface = mem_get_sysvar16(mes_sysvar16_dst_surface);
+	const uint16_t start_x = mem_get_sysvar16(mes_sysvar16_text_start_x);
+	const uint16_t end_x = mem_get_sysvar16(mes_sysvar16_text_end_x);
+	const uint16_t char_space = mem_get_sysvar16(mes_sysvar16_char_space);
+	const uint16_t line_space = mem_get_sysvar16(mes_sysvar16_line_space);
+	uint16_t x = mem_get_sysvar16(mes_sysvar16_text_cursor_x);
+	uint16_t y = mem_get_sysvar16(mes_sysvar16_text_cursor_y);
+	while (*text) {
+		int ch;
+		bool zenkaku = SJIS_2BYTE(*text);
+		uint16_t this_char_space = zenkaku ? char_space : char_space / 2;
+		if (x + this_char_space > end_x + 1) {
+			x = start_x;
+			y += line_space;
+		}
+
+		text = sjis_char2unicode(text, &ch);
+		gfx_text_draw_glyph(x, y, surface, ch);
+		x += this_char_space;
+	}
+	mem_set_sysvar16(mes_sysvar16_text_cursor_x, x);
+	mem_set_sysvar16(mes_sysvar16_text_cursor_y, y);
 }
 
 static void shangrlia_init(void)
@@ -137,6 +451,8 @@ struct game game_shangrlia = {
 	.mem_init = shangrlia_mem_init,
 	.mem_restore = shangrlia_mem_restore,
 	.init = shangrlia_init,
+	.draw_text_zen = shangrlia_draw_text,
+	.draw_text_han = shangrlia_draw_text,
 	.expr_op = { CLASSICS_EXPR_OP },
 	.stmt_op = { CLASSICS_STMT_OP },
 	.sys = {
@@ -146,7 +462,7 @@ struct game game_shangrlia = {
 		[3] = classics_anim,
 		[4] = shangrlia_savedata,
 		[5] = classics_audio,
-		[6] = NULL,
+		[6] = NULL, // unused
 		[7] = sys_file,
 		[8] = sys_load_image,
 		[9] = classics_palette,
@@ -156,13 +472,16 @@ struct game game_shangrlia = {
 		[13] = sys_farcall,
 		[14] = classics_get_cursor_segment,
 		[15] = sys_menu_get_no,
+		[17] = util_noop,
 		[18] = sys_check_input,
+		[19] = NULL, // unused
+		[20] = util_noop,
 		[21] = sys_strlen,
-		[22] = sys_22,
+		[22] = util_noop,
 		[23] = shangrlia_set_speaker,
 	},
 	.util = {
-		[0] = NULL,
+		[0] = util_map,
 		[1] = classics_get_text_colors,
 		[100] = NULL,
 	},
